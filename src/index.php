be3181201c3fe9c7c6b0495968d5d6eb55322df8
Adam Bartosik <codesurf42@users.noreply.github.com>
2017-11-29 11:56:09 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h2>Ecs codepipeline test BG5</h2>
+                <h2>Ecs codepipeline test BG6</h2>
                 <h3>Congratulations</h3>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
